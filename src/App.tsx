c62be4e53b14fc60a80c74f8f12d382cc767ec06
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2024-02-05 21:14:59 +0100

    Starting creating connection to backend, need to solve cors issue, handle timer logic first

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import imageHandler from './imageClickingHandler';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Mark from './Mark';
 import Info from './Info';
 import Timer from './Timer';
@@ -9,12 +9,38 @@ import timeHandler from './timeHandler';
 function App() {
   const [mark, setMark] = useState(null);
   const [wasFound, setWasFound] = useState<boolean | null>(null);
-   const [time, setTime] = useState<number>(0);
+  const [time, setTime] = useState<number>(0);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000');
+        if (!response.ok) {
+          console.log(response);
+          throw new Error('Failed to fetch JWT');
+        }
+        const data = await response.json();
+        const { token } = data;
+
+        localStorage.setItem('jwt', token);
+        setStart(true);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error('Error fetching JWT:', error.message);
+        } else {
+          console.error('Unknown error:', error);
+        }
+      }
+    };
+
+    fetchData();
+  }, []);
 
   function markWaldo(e: MouseEvent, callback): void {
     setMark(callback(e));
     callback(e) ? setWasFound(true) : setWasFound(false);
-    callback(e) ? console.log(`Your record is ${timeHandler(time)}`) : ''
+    callback(e) ? console.log(`Your record is ${timeHandler(time)}`) : '';
     setTimeout(() => {
       setWasFound(null);
     }, 2000);
@@ -23,7 +49,8 @@ function App() {
   return (
     <>
       <h1>Welcome in where's waldo game</h1>
-      <Timer time={time} setTime={setTime}/>
+      {start && <Timer time={time} setTime={setTime} />}
+
       <div style={{ position: 'relative' }}>
         {wasFound !== null ? <Info wasFound={wasFound} /> : ''}
         <img
